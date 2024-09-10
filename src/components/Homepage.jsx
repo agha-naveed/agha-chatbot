e3ccdb79b5bd64dcde1bcdb31cb559a886232b0a
Agha Naveed <naveedabs31@gmail.com>
2024-09-10 18:06:16 +0500

    Remove Unused Hooks

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import DOMPurify from 'dompurify';
 import "@fontsource/poppins"
 import logo from '../assets/my-logo.webp'
