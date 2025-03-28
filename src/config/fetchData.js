e991f9dd36b144780a8e9d00064df5c5319717bc
agha-naveed <naveedabs31@gmail.com>
2025-03-28 22:32:20 +0500

    .

--- src/config/fetchData.js
+++ src/config/fetchData.js
@@ -3,16 +3,16 @@ const run = async(prompt) => {
   const options = {
       method: 'POST',
       headers: {
-          'x-rapidapi-key': process.env.API_KEY ?? "",
-          'x-rapidapi-host': process.env.API_HOST ?? "",
+          'x-rapidapi-key': "67f58cb165mshe057a1e48ceb048p160ec4jsn0a3da1c96fe8",
+          'x-rapidapi-host': "chatgpt-42.p.rapidapi.com",
           'Content-Type': 'application/json'
       },
       body: JSON.stringify({
           messages: [
-              {
-                  role: 'user',
-                  content: prompt
-              }
+                {
+                    role: 'user',
+                    content: prompt
+                }
           ],
           web_access: false
       })
