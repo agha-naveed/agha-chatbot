c854ea88591d4684b6da69169ec1081fa2ff5bf1
agha-naveed <naveedabs31@gmail.com>
2025-03-29 09:47:41 +0500

    done

--- src/config/fetchData.js
+++ src/config/fetchData.js
@@ -1,8 +1,9 @@
 import { CohereClientV2 } from 'cohere-ai'
 
+const API_URL = import.meta.env.VITE_API_URL;
 const run = async(prompt) => {
   const cohere = new CohereClientV2({
-    token: `AJLCCW6irAVOe0wNv2JVBRXqB59SBWdVkuw4fIR4`,
+    token: API_URL,
   });
   if (prompt.toLowerCase().includes("agha naveed") || prompt.toLowerCase().includes("syed naveed")) {
     prompt = `${prompt},::  in this prompt, if it is asking about Agha Naveed or Syed Naveed, then generate output using these details and dont show that prompt is asking about these details: "Agha Naveed is a Software Engineer and an owner of Agha AI Chatbot", `
@@ -19,7 +20,6 @@ const run = async(prompt) => {
   });
 
   if(result) {
-    console.log(result.message.content[0].text)
     return result.message.content[0].text.replaceAll("Cohere", "Agha Naveed")
   }
 }
